ade98e63c9cb439acf29770707abe4f47721cdbc
Pengularity <88403571+Pengularity@users.noreply.github.com>
2024-09-28 21:45:32 +0200

    Add files via upload

---
+++ srcs/requirements/mariadb/tools/create_user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+service mysql start
+
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+mysql -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -e "GRAND ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
---
+++ srcs/requirements/wordpress/conf/config.sh
@@ -0,0 +1,37 @@
+DB_HOST=$WORDPRESS_DB_HOST
+DB_NAME=$MYSQL_DATABASE
+DB_USER=$MYSQL_USER
+DB_PASSWORD=$MYSQL_PASSWORD
+
+WP_URL=$DOMAIN_NAME
+WP_TITLE="Inception Project"
+WP_ADMIN_USER="admin"
+WP_ADMIN_PASSWORD="admin_password"
+WP_ADMIN_EMAIL="admin@example.com"
+WP_USER="author"
+WP_USER_MAIL="author@example.com"
+WP_USER_PASSWORD="user_password"
+
+cd /var/www/wordpress
+
+wp core config --dbhost=$DB_HOST \
+               --dbname=$DB_NAME \
+               --dbuser=$DB_USER \
+               --dbpass=$DB_PASSWORD \
+               --allow-root
+
+wp core install --url=$WP_URL \
+                --title="$WP_TITLE" \
+                --admin_user=$WP_ADMIN_USER \
+                --admin_password=$WP_ADMIN_PASSWORD \
+                --admin_email=$WP_ADMIN_EMAIL \
+                --allow-root
+
+wp user create $WP_USER $WP_USER_MAIL \
+               --role=author \
+               --user_pass=$WP_USER_PASSWORD \
+               --allow-root
+
+cd -
+
+php-fpm8.0 -F
